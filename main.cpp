123ea5f9bc98cad8d55ef4300964914ddc5832e7
Josuejs <josuejs15@hotmail.com>
2020-10-17 12:48:45 -0400

    Updates main to contain example code on how to run SDL

--- main.cpp
+++ main.cpp
@@ -1,72 +1,47 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.*/
+//Using SDL and standard IO
+#include <SDL2\SDL.h>
 #include <iostream>
-#include "car.hpp"
-#include "rectangle.hpp"
-#include "Vector2.hpp"
-
-int main(int argc, char ** argv)
-{    
-    /*
-        char 8 bits (1 byte)
-        short 
-        int 32 bits (4 bytes)
-        float
-        long
-        long long
-        bool
-        void
-        |-------------------------------|
-        | (type) (name) = (val) [addr]  |
-        | int x = 4; (32 bits)          |
-        | int y = 1;                    |        
-        |-------------------------------|
-    */
-
-    int x, y;
-    x = 10;
-    y = 20;
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+int main( int argc, char* args[] )
+{
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
     
-    std::cout << "An int variable takes " << sizeof(x) << " bytes of memory" << std::endl;
-    std::cout << "Rectangle area = " << x * y << std::endl;
-
-    // Object: Instance of a class
-    // rect is an instance of Rectangle
-    Rectangle rect(x, y);
-    Rectangle rect1(200, 100);
-    Rectangle rect2(200, 100);
-    Rectangle rect3(70, 1);
-
-    // dot operator to access public members from our Rectangle class
-    std::cout << "Rectangle area = " << rect.area() << std::endl;
-    std::cout << "Rectangle area = " << rect1.area() << std::endl;
-    std::cout << "Rectangle area = " << rect2.area() << std::endl;
-    std::cout << "Rectangle area = " << rect3.area() << std::endl;
-
-    std::cout << "A Rectangle variable takes " << sizeof(rect) << " bytes of memory" << std::endl;
-
-    // Client code that uses the Car class
-    // Object: Instance of a class
-    // bmw is an instance of Car
-    Car bmw("BMW", 180);
-    Car honda("Honda", 220);
-    Car renault("Renault", 175);
-    Car toyota("Toyota", 0);
-
-    bmw.drive();
-    honda.drive();
-    renault.drive();
-    toyota.drive();
-
-    // Vector2 class client code
-    
-    // Creates new instances of vector class
-    Vector2 vec1(5.0, 2.0);
-    Vector2 vec2(15.0, 20.0);
-    Vector2 vec3(50.0, 23.0);
-
-    // Calls the print function
-    vec1.print();
-    vec2.print();
-    vec3.print();
-
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        std::cout << "SDL could not initialize! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            std::cout << "Window could not be created! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+            //Wait two seconds
+            SDL_Delay( 12000 );
+        }
+    }
+    //Destroy window
+    SDL_DestroyWindow( window );
+    //Quit SDL subsystems
+    SDL_Quit();
     return 0;
 }
