8c3aceffbd3048487b4ab1ea12b416fbaceb5250
Josuejs <josuejs15@hotmail.com>
2020-10-19 15:20:31 -0400

    Update main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
-and may not be redistributed without written permission.*/
-//Using SDL and standard IO
 #include <SDL2\SDL.h>
 #include <iostream>
 //Screen dimension constants
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 int main( int argc, char* args[] )
