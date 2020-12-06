f68bae775b463b65dd4abce20eb3a14f6574c174
Palesker <71268363+Josuejs@users.noreply.github.com>
2020-12-06 00:35:49 -0500

    Update code for Asteroid

--- Asteroid.cpp
+++ Asteroid.cpp
@@ -1,7 +1,7 @@
 // Local Libraries
 #include "Asteroid.hpp"
 #include "App.hpp"
-
+#include "GameObject.hpp"
 
 #include <SDL2/SDL_opengl.h>
 #include <gl\GL.h>
@@ -10,31 +10,20 @@
 
 namespace Engine
 {
-    const size_t NUM_POINTS = 16;
-    const float MIN_SIZE = 25.0f;
-    const float MAX_SIZE = 45.0f;
-
-     // TODO: RR: Get this out of here!
-    inline float wrap(float x, float min, float max)
-    {
-        if(x < min) return max - (min - x);
-        if(x > max) return min + (x - max);
-        return x;
-    }
-
-    // Move this out to a Math 
-    const float PI = 3.141592653;
-
-     inline float randInRange(float min, float max)
+    inline float randInRange(float min, float max)
     {
         return min + (max - min) * (rand() / static_cast<float>(RAND_MAX));
     }
+        
+    const size_t NUM_POINTS = 16;
+    const float MIN_SIZE = 25.0f;
+    const float MAX_SIZE = 45.0f;
+    // Move this out to a Math 
+    const float PI = 3.141592653;
 
-     Asteroid::Asteroid(App* parent)
-        : m_mass(1.0f)
-        , m_angle(0.0f)
-        , m_rotation(120)
-        , m_parent(parent) // TODO: RR: Contemplate using a component based design approach
+    Asteroid::Asteroid(App* parent)
+        : GameObject(1.0f, 0.0f, 120.0f)
+        , m_parent(parent)
     {
         float sizeFactor = 1;
         float min = MIN_SIZE / sizeFactor;
@@ -43,74 +32,19 @@ namespace Engine
         for(size_t idx = 0; idx < NUM_POINTS; ++idx)
         {
             const float radians = (idx / static_cast<float>(NUM_POINTS)) * 2.0f * PI;
-            const float randDist = randInRange(min, max);//min + (max - min) * (rand() / static_cast<float>(RAND_MAX));
-
-            m_points.push_back(Engine::Math::Vector2(sinf(radians) * randDist,
-                                                     cosf(radians) * randDist));
+            const float randDist = min + (max - min) * (rand() / static_cast<float>(RAND_MAX));
+              m_points.push_back(Engine::Math::Vector2(sinf(radians) * randDist,
+                                                       cosf(radians) * randDist));
         }
-        
+       
+        float x = randInRange(-150.0f, 150.0f);
+        float y = randInRange(-150.0f, 150.0f);
+        ApplyImpulse(Engine::Math::Vector2(x, y), m_rotation);
     }
 
-    void Asteroid::ApplyImpulse(Engine::Math::Vector2 impulse)
-    {
-        if(m_mass > 0)
-        {
-            m_velocity.x += (impulse.x / m_mass) * cosf((m_rotation) * (PI / 180));
-            m_velocity.y += (impulse.y / m_mass) * sinf((m_rotation) * (PI / 180));
-        }
-    }
-    
     void Asteroid::Update(float deltaTime)
     {
         m_angle += m_rotation * deltaTime;
-
-        m_position.x += m_velocity.x * deltaTime;
-        m_position.y += m_velocity.y * deltaTime;
-
-        // Calculations for wrap around
-        float halfWidth = m_parent->GetWidth() / 2.0f;
-        float halfHeight = m_parent->GetHeight() / 2.0f;
-
-        float worldMinX = -halfWidth;
-        float worldMaxX = halfWidth;
-
-        float worldMinY = -halfHeight;
-        float worldMaxY = halfHeight;
-
-        m_position.x = wrap(m_position.x, worldMinX, worldMaxX);
-        m_position.y = wrap(m_position.y, worldMinY, worldMaxY);
-    }
-
-    void Asteroid::Render()
-    {
-        glLoadIdentity();
-        glTranslatef(m_position.x, m_position.y, 0.f);
-        glRotatef(m_angle, 0.0f, 0.0f, 1.0f);
-
-        glBegin(GL_LINE_LOOP);
-        std::vector<Engine::Math::Vector2>::iterator it = m_points.begin();
-        for (; it != m_points.end(); ++it)
-        {
-            glVertex2f((*it).x, (*it).y);
-        }
-        glEnd();
-        for (int i = 0; i < 16; i++)
-        {
-            float x = randInRange(-150.0f, 150.0f);
-            float y = randInRange(-150.0f, 150.0f);
-            m_position = Engine::Math::Vector2(x, y);
-            ApplyImpulse(Engine::Math::Vector2(x, y));
-            glLoadIdentity();
-            glTranslatef(m_position.x, m_position.y, 0.f);
-            glRotatef(m_angle, 0.0f, 0.0f, 1.0f);
-
-            glBegin(GL_LINE_LOOP);
-            std::vector<Engine::Math::Vector2>::iterator it = m_points.begin();
-            for (; it != m_points.end(); ++it)
-            {
-                glVertex2f((*it).x, (*it).y);
-            }
-            glEnd();
-        }
+        GameObject::Update(m_parent, deltaTime);
     }
 }
--- Asteroid.hpp
+++ Asteroid.hpp
@@ -3,35 +3,27 @@
 #ifndef ASTEROID_HPP
 #define ASTEROID_HPP
 
-// STL
-#include <vector>
-
+// Asteroids
 #include "Vector2.hpp"
+#include "GameObject.hpp"
 
 namespace Engine
-{
+{    
     class App;
-    class Asteroid
-    {
-        public:
+    class Asteroid : public GameObject
+    {                
+        public:  
+            /* ==========================
+            * PUBLIC FUNCTIONS
+            * ==========================*/
             Asteroid(App* parent);
             void Update(float deltaTime);
-            void Render();
         private:
             /* ==========================
-            * PRIVATE FUNCTIONS
-            * ==========================*/
-            void ApplyImpulse(Engine::Math::Vector2 impulse);
-            /* ==========================
             * MEMBERS
-            * ==========================*/
-            float m_angle;
-            float m_mass;
-            float m_rotation;
-            Engine::Math::Vector2 m_position;
-            Engine::Math::Vector2 m_velocity;
-            std::vector<Engine::Math::Vector2> m_points;
+            * ==========================*/            
             App* m_parent;
     };
-} // namespace Engine
+}
+
 #endif
